2d6d1b477c2b738eedc3a8570b388d5e3849322c
Shameera17 <srcarrim@students.nsbm.lk>
2020-04-04 08:11:16 +0530

    user and admin models added

---
+++ models/admin.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+        username: {
+                type: String,
+                required: true
+        },
+
+        email: {
+                type: String,
+                required: true,
+                trim: true,
+                unique: true
+        },
+
+        password: {
+                type: String,
+                required: true,
+                minlength: 6,
+                maxlength: 20
+        }
+},
+{
+        timestamps: true,
+});
+
+const admin = mongoose.model('admin', UserSchema);
+
+module.exports = admin;
---
+++ models/user.model.js
@@ -0,0 +1,121 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    //user name
+    name : {
+        fname : {
+            type: String, 
+            required: true,
+            trim: true
+        },
+        mname : {
+            type: String, 
+            required: true,
+            trim: true
+        },
+        lname : {
+            type: String, 
+            required: true,
+            trim: true
+        }
+    },
+
+    NIC : {
+        type: String, 
+        required: true,
+        trim: true,
+        unique :true
+    },
+
+    DOB : {
+        type: Date, 
+        required: true,
+    },
+
+    address : {
+        line1 : {
+            type: String,
+            required: true
+        },
+        city : {
+            type: String,
+            required: true
+        },
+        district : {
+            type: String, 
+            required: true
+        },
+        country : {
+            type: String, 
+            required: true
+        },
+        postalCode: {
+            type: Number, 
+            required: true
+        }
+    },
+
+    contactNo : {
+        type: String, 
+        required: true,
+        trim: true
+    },
+
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    profession : {
+        type: String, 
+        required: true
+    },
+
+    password : {
+        type: String, 
+        required: true,
+        minlength: 6
+        
+    },
+
+    DateJoined : {
+        type: Date,
+        'default' : Date.now
+    },
+    
+    ProfileImage : {
+        data:Buffer,
+        contentType: String
+    },
+
+    CoverImage : {
+        data:Buffer,
+        contentType: String
+    },
+
+    EventJoined :[{
+            EventID : {
+                type: Schema.Types.ObjectID,
+                ref : "Event",
+                required : true    
+            },
+            EventJoinedDate : {
+                type: Date, 
+                'default' : Date.now
+            }
+        }
+        ]
+    
+},
+{
+    timestamps: true
+}
+);
+
+const user = mongoose.model('user', UserSchema);
+
+module.exports = user;
